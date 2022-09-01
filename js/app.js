5a366fd4963df0e9adb43f28f4bec85051c62708
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-09-01 10:21:26 -0400

    tasks can now be deleted from local storage

--- js/app.js
+++ js/app.js
@@ -109,13 +109,42 @@ function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         if(confirm('Are You Sure?')){
             e.target.parentElement.parentElement.remove();
+
+            //Remove from LS
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+
+//Remove from LS
+
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //Clear Tasks
 function clearTasks(){
     taskList.innerHTML = '';
+
+
+    //Clear from LS
+    clearTasksFromLocalStorage();
+}
+
+function clearTasksFromLocalStorage(){
+    localStorage.clear();
 }
 
 //Filter Tasks 
